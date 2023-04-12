5e33c69856d50d1bdcee8cba6add03ba3e6e7cc1
Garo <94136201+MagoGaro@users.noreply.github.com>
2023-04-12 11:48:53 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,50 @@
+let areaMensaje = document.getElementById("areaMensaje");
+let areaTexto = document.getElementById("areaTexto");
+
+function ecambio(){
+    let texto = document.getElementById("texto1").value;
+    let txtcif = texto.replace(/e/igm,"enter");
+     txtcif = txtcif.replace(/o/igm,"ober");
+     txtcif = txtcif.replace(/i/igm,"imes");
+     txtcif = txtcif.replace(/a/igm,"zi");
+     txtcif = txtcif.replace(/u/igm,"ufat");
+     return txtcif
+}
+
+function dcambio(){
+    let texto = document.getElementById("texto1").value;
+    let txtcif = texto.replace(/enter/igm,"e");
+     txtcif = txtcif.replace(/ober/igm,"o");
+     txtcif = txtcif.replace(/imes/igm,"i");
+     txtcif = txtcif.replace(/zi/igm,"a");
+     txtcif = txtcif.replace(/ufat/igm,"u");
+     return txtcif
+}
+
+
+function om(){
+       areaMensaje.classList.add('oculto');
+       areaTexto.classList.remove('oculto');
+}
+
+function encriptar(){
+     let txtcif = ecambio();
+     document.getElementById("texto2").innerHTML = txtcif;
+     om();
+}
+
+function desenciptar(){
+    let txtcif = dcambio();
+    document.getElementById("texto2").innerHTML = txtcif;
+    om();
+}
+
+function copiar() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand('copy');
+    alert("Copied!");
+  }
+
+
+
